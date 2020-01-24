8a8cef93df63dbbe677a68c8e68cd5eb7bfe0198
apecero <alan.pecero@agilethought.com>
2020-01-24 10:38:43 -0600

    First Commit

---
+++ src/test/java/SimpleGetTest.java
@@ -0,0 +1,35 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class SimpleGetTest {
+
+    @Test
+    public void GetWeatherDetails(){
+        //Specifying The bae URL to the RESTful web service
+        RestAssured.baseURI = "http://restapi.demoqa.com/utilities/weather/city";
+
+        // Get the RequestSpecification of the request that you want to sent
+        // to the server. The server is specified by the BaseURI that we have
+        // specified in the above step.
+        RequestSpecification httpRequest = RestAssured.given();
+
+
+        // Make a request to the server by specifying the method Type and the method URL.
+        // This will return the Response from the server. Store the response in a variable.
+        Response response = httpRequest.request(Method.GET, "/Hyderabad");
+        int statusCode= response.getStatusCode();
+
+        // Now let us print the body of the message to see what response
+        // we have recieved from the server
+        String responseBody = response.getBody().asString();
+        System.out.println("Response Body is =>  " + responseBody);
+
+        Assert.assertEquals(statusCode /*actual value*/, 200 /*expected value*/, "Correct status code returned");
+
+    }
+}
